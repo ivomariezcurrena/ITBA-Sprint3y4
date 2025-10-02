899ec8d0e147dccb74fb9d094d581e561d1813f0
maylimachi <maylimachi18@gmail.com>
2025-10-02 16:02:01 -0300

    Fetch a /api/productos implementado en App.jsx

--- client/mi-app/src/App.jsx
+++ client/mi-app/src/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [productos, setProductos] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/productos") 
+      .then((res) => {
+        if (!res.ok) throw new Error("Error al obtener productos")
+        return res.json()
+      })
+      .then((data) => {
+        setProductos(data) 
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err.message)
+        setLoading(false)
+      })
+  }, [])
 
   return (
     <>
       <Navbar />
       <main>
-        <h1>Pruebas</h1>
+        <h1>Lista de productos</h1>
+        {loading && <p>Cargando productos...</p>}
+        {error && <p style={{color: "red"}}>Error: {error}</p>}
+
+        {!loading && !error && (
+          <ul>
+            {productos.map((p) => (
+              <li key={p.id}>{p.nombre} - ${p.precio}</li>
+            ))}
+          </ul>
+        )}
       </main>
       <Footer />
     </>
