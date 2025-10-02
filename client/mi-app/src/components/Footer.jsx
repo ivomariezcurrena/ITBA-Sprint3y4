778d78ebb3b6f9e3935b74f3aed29a74d76ac7d0
maylimachi <maylimachi18@gmail.com>
2025-10-01 23:12:30 -0300

    Estructura base en React: integración de Navbar y Footer

---
+++ client/mi-app/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>Email: info@hermanosjota.com.ar</li>
+        <li>Ventas: ventas@hermanosjota.com.ar</li>
+        <li>WhatsApp: +54 11 4567-8900</li>
+        <li>Instagram: @hermanosjota_ba</li>
+      </ul>
+      <p>
+        &copy; 2025 Hermanos Jota. Todos los derechos reservados. 
+        Este manual es propiedad de Hermanos Jota Muebles.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
+
---
+++ client/mi-app/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import './Navbar.css'
+import logo from '../assets/logo.svg'
+
+function Navbar() {
+  return (
+    <nav>
+      <a href="index.html">
+        <img src={logo} alt="logo" />
+
+      </a>
+
+      <ul>
+        <li><a href="/catalogo">PRODUCTOS</a></li>
+        <li><a href="/contacto">CONTACTO</a></li>
+        <li>
+          <a href="#carrito">
+            <i className="fa-solid fa-cart-shopping"></i> (<span id="carrito-count">0</span>)
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
